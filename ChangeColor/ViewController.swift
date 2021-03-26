78a4ccc9fd19d48a9ce5b5ca7e8f9de10ea0893a
Sergeytsev A <Sergeytsev A>
2021-03-26 15:42:19 +0300

    Initial commit

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorScrennView: UIView!
+    
+    @IBOutlet weak var redColorLabel: UILabel!
+    @IBOutlet weak var greenColorLabel: UILabel!
+    @IBOutlet weak var blueColorLabel: UILabel!
+    
+    @IBOutlet weak var redValueColorLabel: UILabel!
+    @IBOutlet weak var greenValueColorLabel: UILabel!
+    @IBOutlet weak var blueValueColorLabel: UILabel!
+    
+    
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+    @IBOutlet weak var blueColorSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
