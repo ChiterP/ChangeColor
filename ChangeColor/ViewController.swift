e652abde4d7677f5cfabbe4cd2a1a420146edd52
Sergeytsev A <Sergeytsev A>
2021-03-26 14:21:12 +0300

    Initial Commit

---
+++ ChangeColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeColor
+//
+//  Created by 18316409 on 26.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
